cca7893aeaed707767220fd101eb6fc698a52cad
TITO-MACBOOK-PC\Tito <htp.tito@gmail.com>
2018-04-28 14:50:32 -0700

    Command Prompt

---
+++ Assets/_Scripts/CommandPrompt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandPrompt : MonoBehaviour 
+{
+	public GameObject promptPrefab;
+	public Transform cmdContent;
+	private InputField currentPrompt;
+
+	public void PromptEntry()
+	{
+		GameObject go = Instantiate (promptPrefab, cmdContent) as GameObject;
+		currentPrompt = go.GetComponentInChildren<InputField>();
+		currentPrompt.Select();
+		currentPrompt.ActivateInputField();
+	}
+
+	public void ExecuteCommand(string command)
+	{
+		Debug.Log (command);
+	}
+}
---
+++ Assets/_Scripts/PromptLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptLine : MonoBehaviour 
+{
+	CommandPrompt cmd;
+	void Start()
+	{
+		cmd = FindObjectOfType<CommandPrompt>();
+	}
+	public void NewEntry(string command)
+	{
+		if (!string.IsNullOrEmpty (command)) 
+		{
+			cmd.ExecuteCommand(command);	
+		}
+		cmd.PromptEntry();
+	}
+}
