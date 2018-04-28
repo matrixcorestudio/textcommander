903381035ec6a1f00a343ef46d59e0d216491752
TITO-MACBOOK-PC\Tito <htp.tito@gmail.com>
2018-04-28 11:06:53 -0700

    Test scene

---
+++ Assets/GroundMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMover : MonoBehaviour 
+{
+	public Transform groundPlane;
+	[Range(0.1f,1f)]
+	public float moveTime = 1f;
+
+	void Start()
+	{
+		StartCoroutine (ScrollWorldRoutine(Vector3.left));
+	}
+
+	IEnumerator ScrollWorldRoutine(Vector3 dir)
+	{
+		while (true) 
+		{
+			groundPlane.position += dir;
+			yield return null;
+			//yield return new WaitForSeconds (moveTime);
+		}
+	}
+}
