8a041ef424a434edcc194b7eee8b4f29ce7e7f8f
TITO-MACBOOK-PC\Tito <htp.tito@gmail.com>
2018-04-28 19:29:16 -0700

    Bullet

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -12,22 +12,26 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         t = GetComponent<RectTransform>();
-        Destroy(gameObject, 3f);
+        Destroy(this.gameObject, 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        t.Translate(Vector3.right * moveForce);
+		t.Translate(Vector3.left * moveForce,Space.Self);
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+		if (collision.CompareTag ("Player")) 
+		{
+			return;
+		}
+        if (collision.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
         }
-        else if (collision.gameObject.CompareTag("Boss"))
+        else if (collision.CompareTag("Boss"))
         {
             collision.gameObject.GetComponent<Boss>().health -= damage;
         }
